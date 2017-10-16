bac311b4c417c88a1471211e57cb46917dc0f238
brasil <brasil@localhost.localdomain>
2017-10-16 20:01:05 -0200

    create product composite and product

--- app/models/productComposite.rb
+++ app/models/productComposite.rb
@@ -1,9 +1,32 @@
-Class Product < ProductComposite
+ProductComposite < AppicationRecord
+belongs_to :category
 
-    def initializer(name)
-        super.('Product product')
-        @category = []
+  def initializer(name)
+    super(name)
+    @category =[]
+  end
 
-        self << ProductComposite.new
-    end
+  def getCategory(name)
+    super(name)
+    @product =[]
+  end
+
+  def add_Category(category)
+    @product << product
+    product.parent = self
+  end
+
+  alias << add_Category
+
+  def remove_Category(category)
+    @category.delete(category)
+    task.parent = nil
+  end
+
+  def [] (index, value)
+    replace_value = @category[index]
+    @category[index] = value
+    replace_value.parent = nil
+    value.parent = self
+  end
 end
